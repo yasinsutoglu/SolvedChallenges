fb459c950a2ca2e77fe3bfb61d545096b6e7ef13
Yasin Sutoglu <ysutoglu@gmail.com>
2022-08-31 18:52:16 +0300

    additions done

--- calisma.js
+++ calisma.js
@@ -834,58 +834,140 @@ console.log(hesapla("LVIII"));
 
 /*************************************/
 
-//1. Bölüm Loops
-
 //! Q-1
 //? Ask the user to input a number. Check the input of the user, if it is not a valid number
 //? then keep getting input from the user until provides a valid number.
 
+// const validNumber = () => {
+//     let num = prompt('enter a valid number');
+
+//     if(Number(num)){
+//         return "It is a valid number";
+//     }else{
+//         return validNumber();
+//     }
+// }
+
+// console.log(validNumber());
 
 //! Q-2
 //? Write a JavaScript program to compute the greatest common divisor (GCD) (OBEB) of two positive integers.
 
+// const findGcd = (num1,num2) => {
+//     let gcd;
+
+//         if(num1 === 0 || num2 === 0){
+//             return 'please enter a valid number';
+//         }
+
+//         for(let i =1; i <= num1 && i<=num2 ;i++){
+//             if(num1%i === 0 && num2 % i === 0){
+//                 gcd = i;
+//             }
+//         }
+
+//     return gcd;
+// }
+
+// console.log(findGcd(3,6))
 
 //! Q-3
 //? Write a code that displays the factors (tam bolen) of a number entered by the user.
 
+// const factor = (num) => {
+//     let arr = [];
+//     for(let i =1; i<=num;i++){
+//         if(num%i === 0){
+//             arr.push(i,-i)
+//          }
+//     }
+//     return arr;
+// }
+
+// console.log(factor(12))
+
 //! Q-4
 //? Write a code to calculate the factorial of a number entered by the user.
 
+// const factorial = (num) => {
+//     if(num < 0) return -1;
+//     if(num === 0){
+//         return 1;
+//     }else{
+//         return num * factorial(num-1)
+//     }
+// }
+
+// console.log(factorial(4));
+
 //! Q-5
 //? Write a code to find numbers that are divisible by 3 and 5, between 100 and 1000.
+// const divisible = () => {
+//     let arr = [];
+//     for(let i=100; i<1000; i++){
+//         if(!(i%3) && !(i%5)){
+//             arr.push(i);
+//         }
+//     }
+//     return arr;
+// }
 
+// console.log(divisible())
 
 //! Q-6
 //? Write a code that prints a multiplication table of numbers 1 to 10.
 
+// const multiplication = () =>{
+//     let arr = [];
+//         for(let i = 1; i <=10 ; i++){
+//             arr.push(`multiplication table of ${i}`)
+//             for(let k=0; k<=10; k++){
+//                 arr.push(`${i}*${k} = ${i*k}` )
+//             }
+//         }
+//     return arr;
+// }
+
+// console.log(multiplication())
 
 //! Q-7
 //? Write a code to draw Whiteball and Powerball numbers for the Powerball lottery game. 
 //? 5 Whiteball numbers are chosen from 01 to 69 and 1 Powerball number is chosen from 1 to 26.
 
+// const powerball = () => {
+//     let powerball = Math.floor(Math.random() * 26) + 1;
+//     let whiteball = [];
 
+//     for(let i=1; i<=5; i++){
+//         let random = Math.floor(Math.random() * 69) + 1;
+//         whiteball.push(random);
+//     }
 
-//2. Bölüm Functions
+//     return `${whiteball.join(',')} , ${powerball}`
+// }
 
-//! Q-1
+// console.log(powerball())
+
+//! Q-8
 //? Write a Function that takes 2 parameters (amount and rate) to convert currency.
 
-//! Q-2
+
+//! Q-9
 //? Write a JavaScript function that accepts an argument and returns the type.
 
-//! Q-3
+//! Q-10
 //? You are given a string and character. Write a function to check the string and find how many
 //? times this character occurs in this string.
 //? Example:
 //? str = "Clarusway"         char = "a"       ===> result = 2 as "a" occurs 2 times in Clarusway
 
-//! Q-4
+//! Q-11
 //? Write a JS code to display Perfect Numbers from 1 to 1000.
 //? A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
 //? For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
 
 
-//! Q-5
+//! Q-12
 //? Develop a guessing game. Randomly generate a number between 0-100. Ask the user to guess that number.
 //? The user should have 5 chance to guess the number.
 //? If the number entered by the user:
