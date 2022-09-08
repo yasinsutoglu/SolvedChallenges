df18c0f78c7832311310c3264d03d451939bd030
Yasin Sutoglu <ysutoglu@gmail.com>
2022-09-08 18:01:29 +0300

    added solutions

--- challenges.js
+++ challenges.js
@@ -1440,123 +1440,71 @@ const objD = { c: 3 }
 combine(objA, objB) // Returns { a: 13, b: 20, c: 36, d: 3 }
 */
 
-const objA = { a: 10, b: 20, c: 30 };
-const objB = { a: 3, c: 6, d: 3 };
-const objC = { a: 5, d: 11, e: 8 };
-const objD = { c: 3 };
+// const objA = { a: 10, b: 20, c: 30 };
+// const objB = { a: 3, c: 6, d: 3 };
+// const objC = { a: 5, d: 11, e: 8 };
+// const objD = { c: 3 };
 
-function combine(...objs){
-    let result = {};
-    for(const o of objs){
-        for(const [key, val] of Object.entries(o)){
-            if(!result.hasOwnProperty(key)){
-                result[key] = val;
-            }else{
-                result[key] += val;
-            }
-        }
-    }
+// function combine(...objs){
+//     let result = {};
+//     for(const o of objs){
+//         for(const [key, val] of Object.entries(o)){
+//             if(!result.hasOwnProperty(key)){
+//                 result[key] = val;
+//             }else{
+//                 result[key] += val;
+//             }
+//         }
+//     }
 
-   return result; 
-}
-
-console.log(combine(objA, objB));
-
-
-
-/************************************************* */
-
-//  Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
-// Examples:
-// 'abc' =>  ['ab', 'c_']
-//'abcdef' => ['ab', 'cd', 'ef']
-
-
-
-
-//*******************************QUESTION 1********************************************/
-​
-// Write a method, that will get an integer array as parameter and will process every number from this array.
-​
-// Return a new array with processing every number of the input-array like this:
-​
-// If the number has an integer square root, take this, otherwise square the number.
-​
-// Example
-// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
-// Notes
-// The input array will always contain only positive numbers, and will never be empty or null.
-​
-//*******************************QUESTION 2********************************************/
-​
-// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
-// The solution would work like the following:
-// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
-​
-​
-// console.log(getEvenNumbers([2,3,8,9,4,5,6]));
-​
-//*******************************QUESTION 3********************************************/
-​
-//Given an array of integers,find the sum of its positive elements
-// Example:
-​
-// input:[1,3,-5,12,4,-21,3,-2]
-// output:23
-​
-// input:[-3,-5,1,-87]
-// output:1
-​
-// *******************************QUESTION 4********************************************/
-​
-// Write a function that takes an integer as input, 
-// and returns the number of bits that are equal to one in the binary representation of that number.
-//  You can guarantee that input is non-negative.
-​
-// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
-​
-​
-// *******************************QUESTION 5*******************************************/
-​
-// Strong number is the number that the sum of the factorial of its digits is equal to number itself.
-// For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
-// Task
-// Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
-// Notes
-// Number passed is always Positive.
-// Return the result as String
-// Input >> Output Examples
-// strong_num(1) ==> return "STRONG!!!!"
-// Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
-// strong_num(123) ==> return "Not Strong !!"
-// Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong .
-// strong_num(2)  ==>  return "STRONG!!!!"
-// Since the sum of its digits' factorial of 2! = 2 is equal to number itself, then its a Strong.
-// strong_num(150) ==> return "Not Strong !!"
-// Since the sum of its digits' factorial of 1! + 5! + 0! = 122 is not equal to number itself, Then it's Not Strong .
-​
-​
-//************************************************************QUESTION 6***************************** */
-// Complete the solution so that it splits the string into pairs of two characters.
-//  If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
-// Examples:
-// * 'abc' =>  ['ab', 'c_']
-// * 'abcdef' => ['ab', 'cd', 'ef']
-//************************************************************QUESTION 7***************************** */
-//Write a function to accept two arrays. Does the first array contain all elements represented in the second array?
-​
-// const containsAllElements = (firstArray, secondArray) => {
-//     // your code
+//    return result; 
 // }
-​
-// console.log(containsAllElements(["monday", "tuesday"], ["tuesday"])) // false
-// console.log(containsAllElements([1,2,3,4,5,6,78,9,], [1,2,3,5]))  //true
-​
-//************************************************************QUESTION 8***************************** */
-//Write a function that accepts an array as a parameter and returns the first item that smaller than the average of its elements
-​
-// const smallerThanAvg = ([1,2,6]) => {
-//     // your code
+
+// console.log(combine(objA, objB));
+
+
+//*******************************QUESTION ********************************************/
+
+
+// const rootOrSquare = (arr) => {
+//   // return arr.map((item) => (item**0.5)%1 ==0 ? item**0.5 : item**2);
+//   return arr.map((item) => Number.isInteger(item**0.5) ? item**0.5 : item**2);
 // }
-​
-​
+
+// console.log(rootOrSquare([4,3,9,7,2,1]))
+
+
+//*******************************QUESTION ********************************************/
+//Even Numver finding in a number array
+
+// const getEvenNumbers = (arr) => {
+//     return arr.filter((item) => (item%2 ===0) )
+// }
+
+// console.log(getEvenNumbers([4, 3, 9, 7, 2, 1]));
+
+
+//*******************************QUESTION ********************************************/
+
+// const sumOfPositives = (arr) =>{
+//   // return arr.reduce((sum,item) => sum+(item>0 ? item : 0) ,0);
+//   return arr.filter(i => i>0).reduce((sum,item) => sum+=item,0);
+// }
+
+// console.log(sumOfPositives([-1,5,-2,-6, 7, 9, -4]))
+
+// *******************************QUESTION ********************************************/
+
+
+
+
+// *******************************QUESTION *******************************************/
+
+
+//***************************************QUESTION ***************************** */
+
+
+
+//*********************************QUESTION ***************************** */
+
+//***********************************QUESTION ***************************** */
