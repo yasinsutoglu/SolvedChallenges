8df86dd3400d9c6b973ab500325fdac91eb297ea
Yasin Sutoglu <ysutoglu@gmail.com>
2022-09-16 09:50:21 +0300

    diamond done

--- challenges.js
+++ challenges.js
@@ -1711,3 +1711,43 @@ combine(objA, objB) // Returns { a: 13, b: 20, c: 36, d: 3 }
 // console.log(highAndLow("1 2 -3 4 5"));
 
 /************************************************* */
+
+// Write a function n element star diamond 
+
+const diamond = function(n){
+    const m = (2*n)-1;
+    let str = "";
+
+    for(let i=0; i<n;i++){
+        const arr = Array(m).fill(" ");
+        if(i===0){
+            arr.splice((n-1),1,'*', ' ');
+            let arrStr0 = arr.join(" ");
+            str += arrStr0 + "\n"
+        }else{
+            arr.splice(n - (i + 1), 1, "*");
+            arr.splice(n + (i - 1), 1, "*");
+           let arrStr1 = arr.join(" ");
+           str+= arrStr1 + "\n";
+        }        
+    }
+
+    for(let j=n-2;j>=0;j--){
+        const arr = Array(m).fill(" ");
+        if (j === 0) {
+          arr.splice(n-1, 1, "*", " ");
+          let arrStr2 = arr.join(" ");
+          str += arrStr2 + "\n";
+        } else {
+          arr.splice(n - (j + 1), 1, "*");
+          arr.splice(n + (j - 1), 1, "*");
+          let arrStr3 = arr.join(" ");
+          str += arrStr3 + "\n";          
+        }          
+    }
+    return str; 
+}
+
+
+// const star = +prompt("Enter diamond star number");
+console.log(diamond(8));
